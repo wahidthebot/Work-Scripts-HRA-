a20f0ac07030066cc6dcd60a14f0940b285fa63c
Wahid Hussain <65582445+wahidthebot@users.noreply.github.com>
2025-04-10 15:50:18 -0400

    Update 5. Remote access.ps1

--- 5. Remote access.ps1
+++ 5. Remote access.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
     Copyright (c) 2025 Wahid Hussain
     This script is licensed under the MIT License.
 #>
@@ -16,67 +16,87 @@ function Get-UserInput {
 $computerName = Get-UserInput "Please enter the computer name: "
 $userLANID = Get-UserInput "Please enter the user LAN ID: "
 
-# Verify if user is a member of specific groups and add if not
-$groups = @("GS-MFA-IPADusers", "GS-MFA-MACusers", "GS-MFA-NewRadiusAuthentication")
-$notMembers = @()
+# --- GROUP MANAGEMENT SECTION ---
+try {
+    # 1. ADD USER TO HRARDPUsers3 (IF NOT ALREADY A MEMBER)
+    $groupToAdd = "HRARDPUsers3"
+    $user = Get-ADUser -Identity $userLANID -Properties memberof -ErrorAction Stop
+    
+    # Check if user is already in HRARDPUsers3
+    $groupDN = (Get-ADGroup -Identity $groupToAdd -ErrorAction Stop).DistinguishedName
+    if (-not ($user.memberof -contains $groupDN)) {
+        Add-ADGroupMember -Identity $groupToAdd -Members $userLANID -ErrorAction Stop
+        Write-Host "SUCCESS: User added to $groupToAdd."
+    } else {
+        Write-Host "INFO: User is already in $groupToAdd."
+    }
 
-foreach ($group in $groups) {
-    $groupDN = (Get-ADGroup -Identity $group).DistinguishedName
-    if (-not (Get-ADUser -Identity $userLANID -Properties memberof | Where-Object { $_.memberof -contains $groupDN })) {
-        Add-ADGroupMember -Identity $group -Members $userLANID
-        $notMembers += $group
+    # 2. REMOVE USER FROM UNWANTED MFA GROUPS
+    $groupsToRemove = @("GS-MFA-IPADusers", "GS-MFA-MACusers", "GS-MFA-NewRadiusAuthentication")
+    $removedGroups = @()
+
+    foreach ($group in $groupsToRemove) {
+        try {
+            $groupObj = Get-ADGroup -Identity $group -ErrorAction Stop
+            if ($user.memberof -contains $groupObj.DistinguishedName) {
+                Remove-ADGroupMember -Identity $group -Members $userLANID -Confirm:$false -ErrorAction Stop
+                $removedGroups += $group
+                Write-Host "SUCCESS: User removed from $group."
+            }
+        } catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
+            Write-Host "INFO: Group $group does not exist (skipped)."
+        } catch {
+            Write-Host "WARNING: Failed to process $group - $($_.Exception.Message)"
+        }
+    }
+
+    # Summary of group changes
+    if ($removedGroups.Count -gt 0) {
+        Write-Host "SUMMARY: User was removed from: $($removedGroups -join ', ')."
     }
 }
-
-# Inform the user about the groups they were added to
-if ($notMembers.Count -gt 0) {
-    Write-Host "The user was not a part of the following groups and has been added: " -NoNewline
-    $notMembers -join ", "
-} else {
-    Write-Host "The user is already a member of all the required groups."
+catch {
+    Write-Host "ERROR: Group management failed - $($_.Exception.Message)"
+    exit
 }
 
-# Check if the computer exists and ping to check if it's online
+# --- COMPUTER MANAGEMENT SECTION ---
 try {
     $computer = Get-ADComputer -Identity $computerName -ErrorAction Stop
     if (Test-Connection -ComputerName $computerName -Count 1 -Quiet) {
         Write-Host "The computer $computerName is online."
         
         # Remove current users from Remote Desktop Users group and add the specified user
-        Invoke-Command -ComputerName $computerName -ScriptBlock {
-            $group = [ADSI]"WinNT://./Remote Desktop Users,group"
-            $members = @($group.PSBase.Invoke("Members")) | ForEach-Object { $_.GetType().InvokeMember("ADsPath", 'GetProperty', $null, $_, $null) }
-            foreach ($member in $members) {
-                $group.Remove($member)
+        try {
+            Invoke-Command -ComputerName $computerName -ScriptBlock {
+                $group = [ADSI]"WinNT://./Remote Desktop Users,group"
+                $members = @($group.PSBase.Invoke("Members")) | ForEach-Object { $_.GetType().InvokeMember("ADsPath", 'GetProperty', $null, $_, $null) }
+                foreach ($member in $members) {
+                    $group.Remove($member)
+                }
+                $group.Add("WinNT://$using:userLANID,user")
             }
-            $group.Add("WinNT://$using:userLANID,user")
+            Write-Host "SUCCESS: $userLANID added to Remote Desktop Users on $computerName."
+            
+            # Update extensionAttribute2
+            Set-ADUser -Identity $userLANID -Replace @{extensionAttribute2 = "$computerName.windows.nyc.hra.nycnet"}
+            Write-Host "SUCCESS: extensionAttribute2 updated."
+            
+            Write-Host "Close Out Ticket: User has been given access to PC."
+        } catch {
+            Write-Host "ERROR: Remote Desktop configuration failed - $($_.Exception.Message)"
         }
-        Write-Host "The user $userLANID has been added to the Remote Desktop Users group on $computerName."
-        
-        # Update extensionAttribute2 for the user
-        $user = Get-ADUser -Identity $userLANID -Properties extensionAttribute2
-        $user.extensionAttribute2 = "$computerName.windows.nyc.hra.nycnet"
-        Set-ADUser -Identity $userLANID -Replace @{extensionAttribute2 = $user.extensionAttribute2}
-        
-        # Close out ticker
-        Write-Host "Close Out Ticket: User has been given access to PC."
     } else {
-        Write-Host "The computer $computerName is found but offline."
-        
-        # Remove extensionAttribute2 for the user if the computer is offline
+        Write-Host "WARNING: Computer $computerName is found but offline."
         Set-ADUser -Identity $userLANID -Clear extensionAttribute2
-        Write-Host "Extension attribute has been removed."
-        
-        # Close out ticker
-        Write-Host "Close Out Ticket: PC seems to be offline. Please check the connection of the PC."
+        Write-Host "INFO: extensionAttribute2 cleared (computer offline)."
+        Write-Host "Close Out Ticket: PC seems to be offline. Please check the connection."
     }
-} catch {
-    Write-Host "The computer $computerName does not exist."
-    
-    # Remove extensionAttribute2 for the user if the computer does not exist
+} catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
+    Write-Host "ERROR: Computer $computerName does not exist in AD."
     Set-ADUser -Identity $userLANID -Clear extensionAttribute2
-    Write-Host "Extension attribute has been removed."
-    
-    # Close out ticker
+    Write-Host "INFO: extensionAttribute2 cleared (computer not found)."
     Write-Host "Close Out Ticket: PC was not able to be located. Please verify the PC name."
+} catch {
+    Write-Host "ERROR: Computer verification failed - $($_.Exception.Message)"
 }

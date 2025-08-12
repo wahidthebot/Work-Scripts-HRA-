bc3139351aefe878cdb9524506fad9e144df425a
Wahid Hussain <65582445+wahidthebot@users.noreply.github.com>
2025-08-12 14:57:35 -0400

    Update Main.ps1

--- Resources App/Main.ps1
+++ Resources App/Main.ps1
@@ -1,4 +1,4 @@
-﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
